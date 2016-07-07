77194620a93732e49a7643ffe9b3de9b2f8fbe0a
ssliteris <ssliteris@deloitte.com>
2016-07-07 13:54:19 +0300

    no token now returns 403

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ router.use(function (req, res, next) {
         // verifies secret and checks exp
         jwt.verify(token, app.get('superSecret'), function (err, decoded) {
             if (err) {
-                return res.json({success: false, message: 'Failed to authenticate token.'});
+                return res.status(403).send({success: false, message: 'Failed to authenticate token.'});
             } else {
                 // if everything is good, save to request for use in other routes
                 req.decoded = decoded;
