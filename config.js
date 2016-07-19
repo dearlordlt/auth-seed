c76326575200cbde36f1b332e5752181b98baa84
Saras <Saras>
2016-07-19 12:10:39 +0300

    added comment by topic

--- config.js
+++ config.js
@@ -7,9 +7,7 @@ module.exports = {
         '/authenticate',
         '/authenticate/',
         '/api',
-        '/',
-        '/comments',
-        '/comments/'
+        '/'
     ]
 
 };
--- server.js
+++ server.js
@@ -93,8 +93,9 @@ router.get('/', function (req, res) {
             deleteUserById: 'DELETE:    /api/users/:userId  Delete a user.'
         },
         comments: {
-            getAllComments: 'GET:          /api/comments - Get all comments.',
-            createComment: 'POST:          /api/comments - Create a comment.',
+            getAllComments: 'GET:       /api/comments - Get all comments.',
+            createComment: 'POST:       /api/comments - Create a comment.',
+            getCommentsByTopic: 'GET:   /api/comments/:topic - get comments by topic.'
         },
         authenticate: {
             authenticate: 'POST:      /api/authenticate - send username and password to get token'
@@ -105,17 +106,17 @@ router.get('/', function (req, res) {
 /**
  * Characters rest service
  */
-require('./character.rest')(router, Character);
+require('./rest/character.rest.js')(router, Character);
 
 /**
  * Users rest service
  */
-require('./users.rest')(router, User);
+require('./rest/users.rest.js')(router, User);
 
 /**
  * Comments rest service
  */
-require('./comments.rest')(router, Comment);
+require('./rest/comments.rest.js')(router, Comment);
 
 /**
  * Authenticate rest service
